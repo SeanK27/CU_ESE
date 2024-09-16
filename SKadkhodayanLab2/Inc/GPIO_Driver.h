c4ecdf40d9df3fc9a45081bb18939b5e5f94d238
SeanK27 <seankk100@gmail.com>
2024-09-16 17:22:07 -0600

    Created register maps for GPIO and RCC, included other helpful macros

--- SKadkhodayanLab2/Inc/GPIO_Driver.h
+++ SKadkhodayanLab2/Inc/GPIO_Driver.h
@@ -8,6 +8,62 @@
 #ifndef GPIO_DRIVER_H_
 #define GPIO_DRIVER_H_
 
+// Include board properties
+#include "STM32F429i.h"
+
+// GPIO Pin Macros
+#define GPIO_PIN_NUM_0 0
+#define GPIO_PIN_NUM_1 1
+#define GPIO_PIN_NUM_2 2
+#define GPIO_PIN_NUM_3 3
+#define GPIO_PIN_NUM_4 4
+#define GPIO_PIN_NUM_5 5
+#define GPIO_PIN_NUM_6 6
+#define GPIO_PIN_NUM_7 7
+#define GPIO_PIN_NUM_8 8
+#define GPIO_PIN_NUM_9 9
+#define GPIO_PIN_NUM_10 10
+#define GPIO_PIN_NUM_11 11
+#define GPIO_PIN_NUM_12 12
+#define GPIO_PIN_NUM_13 13
+#define GPIO_PIN_NUM_14 14
+#define GPIO_PIN_NUM_15 15
+#define GPIO_PIN_NUM_16 16
+
+// GPIO Pin Mode Macros
+#define GPIO_PinMode_Input 00
+#define GPIO_PinMode_GPOut 01
+#define GPIO_PinMode_Alternate 10
+#define GPIO_PinMode_Analog 11
+
+// GPIO Output Type Macros
+#define GPIO_OPType_PushPull 0
+#define GPIO_OPType_OpenDrain 1
+
+// GPIO Speed Macros
+#define GPIO_Speed_Low 00
+#define GPIO_Speed_Medium 01
+#define GPIO_Speed_High 10
+#define GPIO_Speed_VeryHigh 11
+
+// GPIO Pull-Up Pull-Down Macros
+#define GPIO_PUPDR_None 00
+#define GPIO_PUPDR_PU 01
+#define GPIO_PUPDR_PD 10
+#define GPIO_PUPDR_Reserved 11
+
+
+typedef struct	// GPIO_PinConfig_t struct
+{
+uint8_t PinNumber; 		// Pin Number
+uint8_t PinMode; 		// Pin Mode
+uint8_t OPType; 		// Output Type
+uint8_t PinSpeed; 		// Pin Speed
+uint8_t PinPuPdControl; // Pin Push up/ Pull Down Control
+uint8_t PinAltFunMode; 	// Alternate Function mode
+} GPIO_PinConfig_t;
+
+
 // Initializes the GPIO peripheral based on the configurations provided
 void GPIO_Init(GPIO_RegDef_t * GPIOPort, GPIO_PinConfig_t * PinConfig);
 
@@ -22,5 +78,7 @@ void GPIO_WriteToOutputPin(GPIO_RegDef_t * GPIOPort, uint8_t * WritePin, uint9_t
 
 // Toggles the specified pin in the specified GPIO Port
 void GPIO_ToggleOutputPin(GPIO_RegDef_t * GPIOPort, uint8_t * TogglePin);
+
+
 
 #endif /* GPIO_DRIVER_H_ */
--- SKadkhodayanLab2/Inc/STM32F429i.h
+++ SKadkhodayanLab2/Inc/STM32F429i.h
@@ -10,9 +10,66 @@
 
 #include <stdint.h>
 
-#define RNG_BASE_ADDR 0x50060800
-#define RCC_BASE_ADDR 0x40023800
-#define GPIOG_BASE_ADDR 0x40021800
+// Control Macros
+#define ACTIVE 1
+#define NON_ACTIVE 0
+#define SET 1
+#define RESET 0
+#define ENABLE SET
+#define DISABLE RESET
+
+typedef struct // GPIO_RegDef_t struct
+{
+    volatile uint32_t MODER;        // GPIO port mode register               Address offset: 0x00
+    volatile uint32_t OTYPER;       // GPIO port output type register        Address offset: 0x04
+    volatile uint32_t OSPEEDR;      // GPIO port output speed register       Address offset: 0x08
+    volatile uint32_t PUPDR;        // GPIO port pull-up/pull-down register  Address offset: 0x0C
+    volatile uint32_t IDR;          // GPIO port input data register         Address offset: 0x10
+    volatile uint32_t ODR;          // GPIO port output data register        Address offset: 0x14
+    volatile uint32_t BSRR;         // GPIO port bit set/reset register      Address offset: 0x18
+    volatile uint32_t LCKR;         // GPIO port configuration lock register Address offset: 0x1C
+    volatile uint32_t AFR[2];       // GPIO alternate function registers     Address offset: 0x20-0x24
+} GPIO_RegDef_t;
+
+typedef struct // RCC_RegDef_t struct
+{
+    volatile uint32_t CR;          // 0x00: Clock control register
+    volatile uint32_t PLLCFGR;     // 0x04: PLL configuration register
+    volatile uint32_t CFGR;        // 0x08: Clock configuration register
+    volatile uint32_t CIR;         // 0x0C: Clock interrupt register
+    volatile uint32_t AHB1RSTR;    // 0x10: AHB1 peripheral reset register
+    volatile uint32_t AHB2RSTR;    // 0x14: AHB2 peripheral reset register
+    volatile uint32_t AHB3RSTR;    // 0x18: AHB3 peripheral reset register
+    uint32_t RESERVED0;            // 0x1C: Reserved, 0x1C
+    volatile uint32_t APB1RSTR;    // 0x20: APB1 peripheral reset register
+    volatile uint32_t APB2RSTR;    // 0x24: APB2 peripheral reset register
+    uint32_t RESERVED1[2];         // 0x28 - 0x2C: Reserved
+    volatile uint32_t AHB1ENR;     // 0x30: AHB1 peripheral clock enable register
+    volatile uint32_t AHB2ENR;     // 0x34: AHB2 peripheral clock enable register
+    volatile uint32_t AHB3ENR;     // 0x38: AHB3 peripheral clock enable register
+    uint32_t RESERVED2;            // 0x3C: Reserved, 0x3C
+    volatile uint32_t APB1ENR;     // 0x40: APB1 peripheral clock enable register
+    volatile uint32_t APB2ENR;     // 0x44: APB2 peripheral clock enable register
+    uint32_t RESERVED3[2];         // 0x48 - 0x4C: Reserved
+    volatile uint32_t AHB1LPENR;   // 0x50: AHB1 peripheral clock enable in low-power mode register
+    volatile uint32_t AHB2LPENR;   // 0x54: AHB2 peripheral clock enable in low-power mode register
+    volatile uint32_t AHB3LPENR;   // 0x58: AHB3 peripheral clock enable in low-power mode register
+    uint32_t RESERVED4;            // 0x5C: Reserved, 0x5C
+    volatile uint32_t APB1LPENR;   // 0x60: APB1 peripheral clock enable in low-power mode register
+    volatile uint32_t APB2LPENR;   // 0x64: APB2 peripheral clock enable in low-power mode register
+    uint32_t RESERVED5[2];         // 0x68 - 0x6C: Reserved, 0x68 - 0x6C
+    volatile uint32_t BDCR;        // 0x70: Backup domain control register
+    volatile uint32_t CSR;         // 0x74: Clock control & status register
+    uint32_t RESERVED6[2];         // 0x78 - 0x7C: Reserved
+    volatile uint32_t SSCGR;       // 0x80: Spread spectrum clock generation register
+    volatile uint32_t PLLI2SCFGR;  // 0x84: PLLI2S configuration register
+    volatile uint32_t PLLSAICFGR;  // 0x88: PLLSAI configuration register
+    volatile uint32_t DCKCFGR;     // 0x8C: Dedicated clocks configuration register
+} RCC_RegDef_t;
+
+#define RCC_BASE_ADDR 0x40023800				// RCC peripheral base address
+#define GPIOG_BASE_ADDR 0x40021800				// GPIOG peripheral base address
+#define RCC ((RCC_RegDef_t *) RCC_BASE_ADDR) 	// Create a pointer to the base address of the RCC register
 
 
 #endif /* STM32F429I_H_ */
